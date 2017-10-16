b3d0c8742cc9453dbeeb8090ab5b80b553fe5f76
Ritarita <rita.perova@gmail.comm>
2017-10-16 19:52:35 +0300

    final

--- index.php
+++ index.php
@@ -3,42 +3,37 @@
 <head>
 	<title>Square</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 	<link rel="stylesheet" href="css/main.css">
 
 	<meta charset="utf-8">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!--
-		
-	This is Sass example.
-
-	-->
+	
 </head>
 <body>
 	<div class="container">
-	
+		<div class="col-lg-12">
+			<h1><i class="fa fa-pencil" aria-hidden="true"></i>Drawing Square on Canvas</h1>
+		</div>
 	</div>
 	<div class="container content">
 		<div class="row">
 			<div class="col-lg-6">
-				<h2>Canvas View</h2>
+				<h2><span class="fa fa-square"></span> Canvas View</h2>
 				<canvas  id="canvas" width="500" height="500">
 					
 				</canvas>	
 			</div>
 			<div class="col-lg-6">
-				<h2>Insert details</h2>
+				<h2><span class="fa fa-edit"></span>Draw Square</h2>
 				<input id="width" class="form-control" type="text" placeholder="Width">
 				<input id="height" class="form-control" type="text" placeholder="Height">
-				<input id="button" class="btn btn-success" type="submit" onclick="Box()" name="">
+				<input id="button" class="btn btn-success" value="Submit" type="submit" onclick="Box()">
 			</div>
 		</div>
 		
 	</div>
-	<script
-	  src="https://code.jquery.com/jquery-3.2.1.js"
-	  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
-	  crossorigin="anonymous"></script>
 	<script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 
 canvas = document.getElementById('canvas');
-button = document.getElementById('button');
 
 c = canvas.getContext("2d");
 
-
-
-
-
 function Box() {
-	c.clearRect(0,0,0,0);
+	c.clearRect(0, 0, canvas.width, canvas.height);
+
 	w = document.getElementById('width').value;
 	h = document.getElementById('height').value;
-
 		c.beginPath();
-		
-	   
 		c.rect(250-w/2, 250-h/2, w, h);
 		c.fillStyle = "black";
 		c.fill();
